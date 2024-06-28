2cd48a2ee9dc32fc7d26cb98db1edd85fad69776
N Vanshika <vnizampuram@gmail.com>
2024-06-28 06:03:50 +0530

    Time: 1 ms (99.91%), Space: 45.8 MB (71.87%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,33 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for(int i=0; i<nums.length; i++)
-        {
-            if(nums[i]>max)
-            {
-                max = nums[i];
-            }
-            if(nums[i]<min)
-            {
-                min = nums[i];
-            }
-        }
-        int[] count = new int[max-min+1];
+        int result = 0;
         
         for(int i=0; i<nums.length; i++)
         {
-            int index = nums[i]-min;
-            count[index]++;
+            int num = nums[i];
+            result = result^num;
         }
-        
-        for(int i=0; i<nums.length; i++)
-        {
-            int index = nums[i]-min;
-            
-            if(count[index] == 1)
-            return nums[i];
-        }
-        return -1;
+        return result;
     }
 }
