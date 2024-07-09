b8c6ba7d2016de2dd032b8962b93414e59364860
N Vanshika <vnizampuram@gmail.com>
2024-07-09 21:03:20 +0530

    Time: 763 ms (5.31%), Space: 76.4 MB (8.16%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.java
@@ -0,0 +1,26 @@
+class Solution {
+    public double averageWaitingTime(int[][] customers) {
+        double avg = 0;
+        double tot_sum = customers[0][0];
+        double val = 0;
+
+        for(int i=0; i<customers.length; i++)
+        {
+            if(customers[i][0]<=tot_sum)
+            {
+                tot_sum = tot_sum + customers[i][1];
+                System.out.println(tot_sum);
+                val = val + tot_sum - customers[i][0];
+                System.out.println(val);
+            }
+            else
+            {
+                tot_sum = customers[i][0];
+                tot_sum = tot_sum + customers[i][1];
+                val = val + customers[i][1];
+            }
+            avg = val/customers.length;
+        }
+        return avg;
+    }
+}
