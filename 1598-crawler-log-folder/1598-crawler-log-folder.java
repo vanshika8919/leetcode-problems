81b293644d5a0431d2da3fea71938d5d902a41ac
N Vanshika <vnizampuram@gmail.com>
2024-07-10 15:16:13 +0530

    Time: 21 ms (44.79%), Space: 43.7 MB (5.9%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -2,20 +2,13 @@ class Solution {
     public int minOperations(String[] logs) {
         int count = 0;
 
-        for(int i=0; i<logs.length; i++)
-        {
-            if(logs[i].equals("../") && count>0)
-            {
-                count--;
-                System.out.println(count);
-            }
-            else if(logs[i].equals("./"))
-            {
-                count = count;
-                System.out.println(count);
-            }
-            else
-            {
+        for(int i = 0; i < logs.length; i++) {
+            if (logs[i].equals("../")) {
+                if (count > 0) {
+                    count--;
+                    System.out.println(count);
+                }
+            } else if (!logs[i].equals("./")) {
                 count++;
                 System.out.println(count);
             }
