3a94153df5f57447e1c74cc336dec7f5ee080d5f
N Vanshika <vnizampuram@gmail.com>
2024-09-27 19:45:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int num=0;
+
+        for(int i=0;i<31;i++){
+            int count=0;
+            for(int j=0;j<nums.length;j++){
+                if(cb(nums[j],i)){
+                    count++;
+                }
+            }
+            if(count%3!=0){
+                num|=1<<i;
+            }
+        }
+
+        return num;
+
+    }
+    boolean cb(int a,int b){
+        
+        return (a & (1<<b))==(1<<b);
+    }
+}
