4ffb14aa83e951ec942f82a568d9c92744b70361
N Vanshika <vnizampuram@gmail.com>
2024-09-27 19:45:37 +0530

    Time: 5 ms (44.08%), Space: 45.3 MB (54.14%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int num=0;
 
-        for(int i=0;i<31;i++){
+        for(int i=0;i<=31;i++){
             int count=0;
             for(int j=0;j<nums.length;j++){
                 if(cb(nums[j],i)){
